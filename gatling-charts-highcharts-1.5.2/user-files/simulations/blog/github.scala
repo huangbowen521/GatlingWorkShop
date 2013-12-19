b50103264fd49001916429141def6776f8d410a7
Bowen <huangbowen521@gmail.com>
2013-12-19 15:54:28 +0800

    do search

--- gatling-charts-highcharts-1.5.2/user-files/simulations/blog/github.scala
+++ gatling-charts-highcharts-1.5.2/user-files/simulations/blog/github.scala
@@ -7,21 +7,21 @@ import com.excilys.ebi.gatling.http.Headers.Names._
 import akka.util.duration._
 import bootstrap._
 
-class GoogleSimulation extends Simulation {
+class GithubSimulation extends Simulation {
 	var httpConf = httpConfig.
-	baseURL("https://www.github.com")
+	baseURL("https://github.com")
 
 	var scn = scenario("search in github.com")
 	.exec(
 		http("home page")
 		.get("/")
 		.check(status.is(200)))
-	// .pause(0, 12)
-	// .exec(
-	// 	http("do search")
-	// 	.get("/")
-	// 	.queryParam("q", "Gatling")
-	// 	.check(status.is(200)))
+	.pause(0, 12)
+	.exec(
+		http("do search")
+		.get("/search")
+		.queryParam("q", "gatling")
+		.check(status.is(200)))
 
-	setUp(scn.users(10).ramp(60).protocolConfig(httpConf))
+	setUp(scn.users(3).ramp(10).protocolConfig(httpConf))
 }
